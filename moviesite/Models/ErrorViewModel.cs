cc4b00ed341f7dd3258a5fa2a256080396d2856a
aman.mulla <aman.mulla@MUM-LAP-1677>
2023-01-16 10:06:00 +0530

    Add project files.

---
+++ moviesite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace moviesite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
